6e907b484af5b2ce072f1ba4be0460f01e9c75ee
Ishaq Idris <idrismoha2@ecs.vuw.ac.nz>
2018-07-06 15:34:58 +1200

    added media organisation

--- mac_desktop_cleaner.py
+++ mac_desktop_cleaner.py
@@ -2,15 +2,24 @@ import os
 
 mypath = "/Users/ishaqidris/desktop/" #change to desktop directory
 desktop = os.listdir(mypath)
+
+#add output folders
 screenshot_folder = "/Users/ishaqidris/desktop/screenshots/"
+media_folder = "/Users/ishaqidris/desktop/media/"
 
 for file in desktop:
     
     #Tidy up screenshot files
     if file.startswith("Screen Shot"):
-        print (file)
         if not os.path.exists(screenshot_folder):
             os.makedirs(screenshot_folder)
 
         os.rename(mypath + file, screenshot_folder + file )
+
+    #Tidy up media files
+    elif file.lower().endswith((('.png', '.jpg', '.mp4', '.mov' ))):
+        if not os.path.exists(media_folder):
+            os.makedirs(media_folder)
+        
+        os.rename(mypath + file, media_folder + file )
 
